5ead336ea91e15b002d6622be04509e40489f59f
pawan cheke <pawancheke11@gmail.com>
2022-09-05 22:45:56 +0530

    variables problems

---
+++ variable-and-data-types/program2.js
@@ -0,0 +1,6 @@
+let name="Pawan cheke";
+console.log(name);
+let fatherName="Achyutrao cheke";
+console.log(fatherName);
+let mother_name="Suman cheke";
+console.log(mother_name);
---
+++ variable-and-data-types/program4.js
@@ -0,0 +1,18 @@
+let name="ｐａｗａｎ ｃｈｅｋｅ";
+ let school="    🅼 🅰  🆂 🅰  🅸   🆂 🅲 🅷 🅾  🅾  🅻";
+  let section="sᴇᴄᴛɪᴏɴ=ғw22_1129";
+   let rollno="Rollno=48";
+    let marks="      subject   marks ";
+    let HTML="       HTML      0";
+     let CSS="       CSS       0";
+      let JS="       JS        0";
+
+console.log(name);
+ console.log(school);
+  console.log(section);
+   console.log(rollno);
+    console.log(marks);
+     console.log(HTML);
+      console.log(CSS);
+       console.log(JS);
+
