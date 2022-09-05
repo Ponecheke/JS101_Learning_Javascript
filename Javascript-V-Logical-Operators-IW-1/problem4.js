e8f81dc0d4f1b94063e7db958ef905ae585985c7
pawan cheke <pawancheke11@gmail.com>
2022-09-05 23:04:34 +0530

    logical operator problems

---
+++ Javascript-V-Logical-Operators-IW-1/problem4.js
@@ -0,0 +1,15 @@
+//Given 3 numbers (all different values), print which is greatest
+
+let a=453;
+let b=55;
+let c=53;
+
+if(a>b && a>c){
+  console.log(a+" is greatest");
+}
+else if(b>a && b>c){
+  console.log(b+" is greatest");
+}
+else if(c>a && c>b){
+  console.log(c+" is greatest");
+}
